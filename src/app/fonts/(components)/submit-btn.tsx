609ecfaf228a8c5407c14ee51ac2f5982e92ea07
Tanzim Mokammel <mtanzim@gmail.com>
2024-10-09 20:38:16 -0400

    basic csv parsing

---
+++ src/app/fonts/(components)/submit-btn.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+export function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button type="submit" disabled={pending}>
+      Add
+    </button>
+  );
+}
