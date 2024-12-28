3d46554ab95747828bc574f8a10d88bc0338d748
Tanzim Mokammel <20617008+mtanzim@users.noreply.github.com>
2024-12-28 02:46:46 -0500

    fix: add auth to export and persist  (#13)

--- src/app/api/persist/route.ts
+++ src/app/api/persist/route.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { getDBClient } from "@/db";
 import { expenseSchemaNonEmpty } from "@/lib/schemas";
+import { withAuth } from "../with-auth";
 export const dynamic = "force-dynamic";
 
 const persistArgsSchema = z.object({
@@ -10,12 +11,11 @@ const persistArgsSchema = z.object({
 
 export type PersistArgs = z.infer<typeof persistArgsSchema>;
 
-// https://help.realbyteapps.com/hc/en-us/articles/360043223253-How-to-import-bulk-data-by-Excel-file
-export async function POST(request: Request) {
+export const POST = withAuth(async (request: Request) => {
   const body = persistArgsSchema.parse(await request.json());
   console.log(JSON.stringify(body, null, 2));
   const client = getDBClient();
   const res = await client.persistMonth(body.month, body.expenses);
   console.log(res);
   return new Response("OK");
-}
+});
